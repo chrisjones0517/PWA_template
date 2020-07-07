a894d874b21b0bfe91689d4dc88d58652d6e46f6
Chris <chrisjones0517@gmail.com>
2020-07-07 15:06:21 -0500

    stuff

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ function console_log($output, $with_script_tags = true)
 
 include_once "index.html";
 
-console_log($_SERVER['ORIG_URI']);
+console_log($_SERVER['REDIRECT_URL']);
 
 // echo ("request uri\n" . $_SERVER['REQUEST_URI']);
 
