aaf111cff0da292febd7a0b0b55e37a55dbaec31
Chris <chrisjones0517@gmail.com>
2020-07-07 13:26:57 -0500

    stuff

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
-if (!isset($_SERVER["HTTPS"]) || $_SERVER != "on") {
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], true, 301);
+if ($_SERVER["HTTPS"] != "on") {
+
+    $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header("Location:$redirect");
 
     exit;
+} else {
+    include_once "index.html";
 }
-
-include_once "index.html";
