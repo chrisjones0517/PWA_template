2bf0361199f6fb5a78e56b64f7c8704b5a5a9b09
Chris <chrisjones0517@gmail.com>
2020-07-07 14:59:08 -0500

    stuff

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ function console_log($output, $with_script_tags = true)
 
 include_once "index.html";
 
-console_log('request_uri', $_SERVER);
+console_log($_SERVER['REQUEST_URI']);
 
 // echo ("request uri\n" . $_SERVER['REQUEST_URI']);
 
