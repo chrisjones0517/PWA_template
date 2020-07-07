f0296b0b945b2a82d5a4e8f08e2f72d725a0a0da
Chris <chrisjones0517@gmail.com>
2020-07-07 12:03:01 -0500

    stuff

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php include_once "index.html";
+<?php
+
+if (!isset($_SERVER["HTTPS"]) || $_SERVER != "on") {
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], true, 301);
+
+    exit;
+}
+
+include_once "index.html";
