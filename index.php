cb3d20ca6cb2656e722724636cdcba197ea59206
Chris <chrisjones0517@gmail.com>
2020-07-07 13:57:30 -0500

    stuff

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-
+include_once "index.html";
 echo ("request uri\n" . $_SERVER['REQUEST_URI']);
 
-if ($_SERVER["HTTPS"] != "on") {
+// if ($_SERVER["HTTPS"] != "on") {
 
-    $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header("Location:$redirect");
+//     $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+//     header("Location:$redirect");
 
-    exit;
-} else {
-    include_once "index.html";
-}
+//     exit;
+// } else {
+//     include_once "index.html";
+// }
